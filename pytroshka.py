8491668948fd874eaffa7af2aa8981449bc56b00
hutchYy <julian.dotreppe@gmail.com>
2020-05-01 19:00:29 +0200

    Added argsparse Reformatted the code to classes Added gitignore

--- pytroshka.py
+++ pytroshka.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-
-
+import argparse
 import os
 import base64
 import bz2
@@ -11,123 +10,172 @@ import tarfile
 import magic
 import subprocess
 
-#       ___               __        ___     __                   ___  __               
-#|  | |  |  |__|    |    /  \ \  / |__     |__) \ /    |__| |  |  |  /  ` |__| \ / \ / 
-#|/\| |  |  |  |    |___ \__/  \/  |___    |__)  |     |  | \__/  |  \__, |  |  |   |                                                                                        
+#        ___               __        ___     __                   ___  __
+# |  | |  |  |__|    |    /  \ \  / |__     |__) \ /    |__| |  |  |  /  ` |__| \ / \ /
+# |/\| |  |  |  |    |___ \__/  \/  |___    |__)  |     |  | \__/  |  \__, |  |  |   |
 
 
-i = 0
-current_file = "1819.gz"
-flagprefix = "rtcp"
+class John(object):
+    def __init__(self, johnCmd, john2zipCmd):
+        self.johnCmd = johnCmd
+        self.john2zipCmd = john2zipCmd
 
-def getPasswordFromJohn():
-    cmd = subprocess.Popen(["john", "--show", "tmp"],
-                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout, _ = cmd.communicate()
-    stdout = str(stdout.decode("utf-8"))
-    if("password hashes cracked" in stdout):
-        return ""
-    elif("No such file or directory" in stdout):
-        return ""
-    else:
-        return stdout.split(":")[1]
+    def zip2john(self, current_file: str, tmp_file: str):
+        cmd = subprocess.Popen(["zip2john", current_file],
+                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = cmd.communicate()
+        if (stderr != "None"):
+            with open(tmp_file, "w", encoding='utf8') as f:
+                f.write(stdout.decode("utf-8"))
 
+    def crackzip(self, tmp_file: str, wordlist: str):
+        cmd = subprocess.Popen(["john", tmp_file, "--wordlist="+wordlist],
+                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = cmd.communicate()
+        return stdout, stderr
 
-def johnZip(tmp_file: str):
-    cmd = subprocess.Popen(["john", tmp_file, "--wordlist=../xato-net-10-million-passwords-100.txt"],
-                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout, stderr = cmd.communicate()
-    return stdout, stderr
-
-
-def zip2john(current_file: str, tmp_file: str):
-    cmd = subprocess.Popen(
-        ["zip2john", current_file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout, stderr = cmd.communicate()
-    if (stderr != "None"):
-        with open(tmp_file, "w", encoding='utf8') as f:
-            f.write(stdout.decode("utf-8"))
-
-
-def unzip(current_file: str, password=""):
-    with zipfile.ZipFile(current_file) as cf:
-        if len(cf.namelist()) == 1:
-            file_to_be_extracted = cf.namelist()[0]
+    def getPasswordFromJohn(self):
+        cmd = subprocess.Popen(["john", "--show", "tmp"],
+                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, _ = cmd.communicate()
+        stdout = str(stdout.decode("utf-8"))
+        if("password hashes cracked" in stdout):
+            return ""
+        elif("No such file or directory" in stdout):
+            return ""
         else:
-            print("[!] Too much files into the archive!")
-            return
-        try:
-            if(password != ""):
-                cf.setpassword(password.encode("utf-8"))
-            cf.extractall()
-            os.rename(file_to_be_extracted, new_file)
-            return True
-        except RuntimeError as e:
-            if ("password" in str(e)):
-                zip2john(current_file, "tmp")
-                passwd = getPasswordFromJohn()
-                if(passwd != ""):
-                    unzip(current_file, passwd)
-                else:
-                    johnZip("tmp")
-                    passwd = getPasswordFromJohn()
-                    if(passwd != ""):
-                        unzip(current_file, passwd)
+            return stdout.split(":")[1]
 
 
-while True:
-    new_file = "decompressed-" + str(i)
+class Pytroshka(object):
+    def __init__(self, pathToArchive, flagPrefix, johnCmd, john2zipCmd, wordlist):
+        self.pathToArchive = pathToArchive
+        self.flagPrefix = flagPrefix
+        self.john = John(johnCmd, john2zipCmd)
+        self.wordlist = wordlist
 
-    # Analyzing file type.
-    file_type = magic.from_file(current_file)
-    print("[*] File '{}' is '{}'.".format(current_file, file_type))
+    def run(self):
+        i = 0
+        while True:
+            new_file = "decompressed-" + str(i)
+            # Analyzing file type.
+            file_type = magic.from_file(self.pathToArchive)
+            print("[*] File '{}' is '{}'.".format(self.pathToArchive, file_type))
+            # Analyzing archives.
+            if "bzip2" in file_type:
+                self.unBzip2(new_file)
 
-    # Analyzing archives.
+            elif "POSIX tar archive (GNU)" in file_type:
+                self.unTar(new_file)
 
-    if "bzip2" in file_type:
-        with open(new_file, 'wb') as nf, open(current_file, 'rb') as cf:
+            elif "Zip" in file_type:
+                self.unzip(self.pathToArchive)
+
+            elif "ASCII text" in file_type:
+                if (self.handleFlag(new_file)):
+                    flag = ""
+                    with open(new_file, "r") as nf:
+                        flag = nf.read()
+                    return flag
+
+            elif "gzip" in file_type:
+                self.unGzip(new_file)
+
+            else:
+                print("[!] Unknown archive, exiting.")
+                break
+
+            # Removing old file and going on with analysis.
+            os.remove(self.pathToArchive)
+            self.pathToArchive = new_file
+            i += 1
+
+    def unBzip2(self, new_file: str):
+        with open(new_file, 'wb') as nf, open(self.pathToArchive, 'rb') as cf:
             decompressor = bz2.BZ2Decompressor()
             for data in iter(lambda: cf.read(100 * 1024), b''):
                 nf.write(decompressor.decompress(data))
 
-    elif "POSIX tar archive (GNU)" in file_type:
-        with tarfile.open(current_file, 'r') as t:
+    def unTar(self, new_file):
+        with tarfile.open(self.pathToArchive, 'r') as t:
             names = t.getnames()
             for name in names:
                 try:
-                    f = t.extract(name)
+                    t.extract(name)
                     os.rename(name, new_file)
                 except KeyError:
                     print('ERROR: Did not find {} in tar archive'.format(name))
 
-    elif "Zip" in file_type:
-        unzip(current_file)
+    def unzip(self, new_file: str, password=""):
+        with zipfile.ZipFile(self.pathToArchive) as cf:
+            if len(cf.namelist()) == 1:
+                file_to_be_extracted = cf.namelist()[0]
+            else:
+                print("[!] Too much files into the archive!")
+                return
+            try:
+                if(password != ""):
+                    cf.setpassword(password.encode("utf-8"))
+                cf.extractall()
+                os.rename(file_to_be_extracted, new_file)
+                return True
+            except RuntimeError as e:
+                if ("password" in str(e)):
+                    self.john.zip2john(self.pathToArchive, "tmp")
+                    passwd = self.john.getPasswordFromJohn()
+                    if(passwd != ""):
+                        self.unzip(self.pathToArchive, passwd)
+                    else:
+                        self.john.crackzip("tmp", self.wordlist)
+                        passwd = self.john.getPasswordFromJohn()
+                        if(passwd != ""):
+                            self.unzip(self.pathToArchive, passwd)
 
-    elif "ASCII text" in file_type:
-        with open(current_file, "r") as cf:
-            encoded_data = cf.read()
-        if(flagprefix in encoded_data):
-            print(encoded_data)
-            break
-        else:
-            decoded_data = base64.b64decode(encoded_data)
-            if(flagprefix in decoded_data):
-                print(decoded_data)
-                with open(new_file, "wb") as nf:
-                    nf.write(decoded_data)
-                break
-
-    elif "gzip" in file_type:
-        with gzip.open(current_file, "r") as cf:
+    def unGzip(self, new_file: str):
+        with gzip.open(self.pathToArchive, "r") as cf:
             read_data = cf.read()
-        with open(new_file, "wb") as nf:
-            nf.write(read_data)
+            with open(new_file, "wb") as nf:
+                nf.write(read_data)
 
-    else:
-        print("[!] Unknown archive, exiting.")
-        break
+    def handleFlag(self, new_file: str):
+        with open(self.pathToArchive, "r") as cf:
+            encoded_data = cf.read()
+            if(self.flagPrefix in encoded_data):
+                print(encoded_data)
+                return True
+            else:
+                decoded_data = base64.b64decode(encoded_data)
+                if(self.flagPrefix in decoded_data):
+                    print(decoded_data)
+                    with open(new_file, "wb") as nf:
+                        nf.write(decoded_data)
+                    return True
+                else:
+                    return False
 
-   # Removing old file and going on with analysis.
-    os.remove(current_file)
-    current_file = new_file
-    i += 1
+
+def parseCmd():
+    parser = argparse.ArgumentParser(
+        description='Simple uncompression tool for nested archives.')
+    parser.add_argument('-a', action='store',
+                        dest='pathToArchive', help='Path to archive')
+    parser.add_argument('-p', action='store', dest='flagPrefix',
+                        help='Flag prefix to stop if the flag is found')
+    parser.add_argument('-w', action='store',
+                        dest='pathTowordlist', help='Path to wordlist')
+    parser.add_argument('-john', action='store', dest='johnCmd',
+                        help='Alias of john', default="john")
+    parser.add_argument('-john2zip', action='store', dest='john2zipCmd',
+                        help='Alias of john2zip', default="john2zip")
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s 1.0')
+    args = parser.parse_args()
+    return args.pathToArchive, args.flagPrefix, args.johnCmd, args.john2zipCmd, args.pathTowordlist
+
+
+if __name__ == "__main__":
+    pathToArchive, flagPrefix, johnCmd, john2zipCmd, wordlist = parseCmd()
+    pytroshka = Pytroshka(pathToArchive, flagPrefix,
+                          johnCmd, john2zipCmd, wordlist)
+    flag = pytroshka.run()
+    print(f"Here is your flag : {flag}")
